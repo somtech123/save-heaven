3a856e82adc972038fcfe5b9bb8f78be08a8065b
oscar onyenacho <oscardre8@gmail.com>
2024-05-18 19:32:56 +0100

    .

--- lib/src/ui/feature/dashboard/presentation/view/dashboard.dart
+++ lib/src/ui/feature/dashboard/presentation/view/dashboard.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/cupertino.dart';
+import 'package:save_heaven/src/utils/utils.dart';
+import 'package:pull_down_button/pull_down_button.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
@@ -6,7 +10,56 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: SafeArea(child: Column()),
+      backgroundColor: Appcolors.whiteColor,
+      navigationBar: CupertinoNavigationBar(
+          middle: const Text('Home'),
+          backgroundColor: Appcolors.whiteColor,
+          trailing: PullDownButton(
+            itemBuilder: (context) => [
+              PullDownMenuActionsRow.medium(
+                items: [
+                  PullDownMenuItem(
+                    onTap: () {},
+                    title: 'Reply',
+                    icon: CupertinoIcons.arrowshape_turn_up_left,
+                  ),
+                  PullDownMenuItem(
+                    onTap: () {},
+                    title: 'Copy',
+                    icon: CupertinoIcons.doc_on_doc,
+                  ),
+                  PullDownMenuItem(
+                    onTap: () {},
+                    title: 'Edit',
+                    icon: CupertinoIcons.pencil,
+                  ),
+                ],
+              ),
+              const PullDownMenuDivider.large(),
+              PullDownMenuItem(
+                title: 'Menu item',
+                onTap: () {},
+              ),
+              const PullDownMenuDivider(),
+              PullDownMenuItem(
+                title: 'Menu item 2',
+                onTap: () {},
+              ),
+            ],
+            buttonBuilder: (context, showMenu) => CupertinoButton(
+              onPressed: showMenu,
+              padding: EdgeInsets.zero,
+              child: const Icon(CupertinoIcons.ellipsis_circle),
+            ),
+          )
+          //  IconButton(
+          //   onPressed: () {
+
+          //   },
+          //   icon: const Icon(CupertinoIcons.settings),
+          // )
+          ),
+      child: const SafeArea(child: Column()),
     );
   }
 }
